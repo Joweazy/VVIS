bf5954b6cc42f8903f1ab596bbf6a0129e2884bb
joeyd <joeydejong89@gmail.com>
2025-10-14 23:08:03 +0200

    undo

--- VVIS.Server/Program.cs
+++ VVIS.Server/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient();
 
-var port = "8080";
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 var app = builder.Build();
